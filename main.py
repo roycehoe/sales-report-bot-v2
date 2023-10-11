e25361f9d40578ffd625858e6d3e617b48cbf27d
royce <royceinbox@hotmail.com>
2023-10-11 22:37:48 +0800

    feat: Create bot that says hello when command hello is given

--- main.py
+++ main.py
@@ -1,1 +1,20 @@
-print("hello world")
+from dotenv import dotenv_values
+from telegram import Update
+from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes
+
+CONFIG = dotenv_values()
+
+TOKEN = CONFIG.get("TOKEN", "") or ""
+
+
+async def hello(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+    if not update.message:
+        return
+    await update.message.reply_text(f"Hello {update.effective_user.first_name}")
+
+
+app = ApplicationBuilder().token(TOKEN).build()
+
+app.add_handler(CommandHandler("hello", hello))
+
+app.run_polling()
