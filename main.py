b24b87db58057b3b47ddb071a9eb98211babb5b4
royce <royceinbox@hotmail.com>
2023-10-12 23:10:57 +0800

    feat: BREAKING Create fully functioning application that reads and prints messages

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-import datetime
+from dataclasses import dataclass
+from datetime import datetime
 from dotenv import dotenv_values
 from telegram import Message, Update
 from telegram.ext import (
@@ -25,11 +26,70 @@ class MessageStore:
 
     def filter_by_date(
         self,
-        date: datetime.datetime = datetime.datetime.today(),
+        date: datetime = datetime.today(),
     ):
         return [
             message for message in self.messages if message.date.date() == date.date()
         ]
+
+
+def get_date_display(date: datetime) -> str:
+    return f"Report date: {date.strftime('%d/%m/%Y')}"
+
+
+def get_banner_display() -> str:
+    return f"=================================="
+
+
+def get_ledger_entry_display(message: Message) -> str:
+    return f"{message.date.strftime('%H:%M:%S')} | {message.from_user.username} | {message.text}"
+
+
+def get_ledger_display(messages: list[Message]) -> str:
+    ledger_entries = [get_ledger_entry_display(message) for message in messages]
+    ledger_display_header = "Time | Username | Payment"
+    ledger = [ledger_display_header, *ledger_entries]
+    return "\n".join(ledger)
+
+
+@dataclass
+class LedgerSummary:
+    username: str
+    payment: list[str]
+
+    def __str__(self):
+        try:
+            total_payment = sum(int(i) for i in self.payment)
+            return f"{self.username} | {total_payment}"
+        except Exception:
+            return f"{self.username} | Unable to parse payments"
+
+
+def get_ledger_summary(messages: list[Message]) -> list[LedgerSummary]:
+    ledger_summary: list[LedgerSummary] = []
+    usernames = list({message.from_user.username for message in messages})
+    for username in usernames:
+        payment = [
+            message.text for message in messages if message.from_user == username
+        ]
+        ledger_summary.append(LedgerSummary(username=username, payment=payment))
+    return ledger_summary
+
+
+def get_ledger_summary_display(messages: list[Message]) -> str:
+    ledger_summary_header = "Username | Payment"
+    ledger_summary = [str(i) for i in get_ledger_summary(messages)]
+    ledger = [ledger_summary_header, *ledger_summary]
+    return "\n".join(ledger)
+
+
+def get_display(messages: list[Message], date: datetime = datetime.today()) -> str:
+    banner_display = get_banner_display()
+    date_display = get_date_display(date)
+    ledger_display = get_ledger_display(messages)
+    ledger_summary_display = get_ledger_summary_display(messages)
+
+    return f"{date_display} \n {banner_display} \n {ledger_display} \n {ledger_summary_display}"
 
 
 class MessageFormatter:
@@ -38,7 +98,9 @@ class MessageFormatter:
 
     async def show(self, update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
         if message := update.message:
-            await message.reply_text(f"{message_store.messages}")
+            display = get_display(message_store.messages)
+            await message.reply_text(f"{display}")
+            # await message.reply_text(f"{message_store.messages}")
 
 
 application = Application.builder().token(TOKEN).build()
